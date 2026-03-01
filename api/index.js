1e061bcf1dc84dcadfa357931e9c3aa5df6ca2ea
nirgtm <nirgtm@users.noreply.github.com>
2026-03-01 20:04:29 +0530

    Add Vercel configuration files

--- api/index.js
+++ api/index.js
@@ -1,5 +1,2 @@
 // Vercel serverless function entry point
-const app = require('../server');
-
-// Export as serverless function handler
-module.exports = app;
+module.exports = require('../server');
