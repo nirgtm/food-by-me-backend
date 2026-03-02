7d3c70baebc8d99e2a3627b78f644bf1dedd3952
nirgtm <nirgtm@users.noreply.github.com>
2026-03-02 15:19:25 +0530

    Update Vercel configuration

--- api/index.js
+++ api/index.js
@@ -1,7 +1,4 @@
 // Vercel serverless function entry point
 const app = require('../server');
 
-// Export the Express app as a serverless function
-module.exports = (req, res) => {
-  return app(req, res);
-};
+module.exports = app;
